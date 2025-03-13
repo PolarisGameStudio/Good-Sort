65d80e23fd4968c86208f0175a9ea35ea942ed48
NhanLe1999 <55460318+NhanLe1999@users.noreply.github.com>
2025-03-13 10:18:23 +0700

    update

--- Assets/Assets/Scripts/Assembly-CSharp/UI_Setting.cs
+++ Assets/Assets/Scripts/Assembly-CSharp/UI_Setting.cs
@@ -22,33 +22,52 @@ public class UI_Setting : Dialog<UI_Setting>
 
 	public Action callbackShowSetting = null;
 
+    private Image imgSound = null;
+    private Image imgMusic = null;
+    private Image imgVirabtion = null;
+
+
     private void Start()
 	{
-	}
+        imgSound = btnSound.GetComponent<Image>();
+        imgMusic = btnMusic.GetComponent<Image>();
+        imgVirabtion = btnHaptic.GetComponent<Image>();
+
+
+        UpdateUI_Setting();
+
+        btnSound.onClick.AddListener(() => {
+            Audio.soundEnabled = !Audio.soundEnabled;
+            UpdateUI_Setting();
+        });
+
+        btnHaptic.onClick.AddListener(() => {
+            HelperManager.DataPlayer.isVbration = !HelperManager.DataPlayer.isVbration;
+            UpdateUI_Setting();
+        });
+
+        btnMusic.onClick.AddListener(() => {
+            Audio.musicEnabled = !Audio.musicEnabled;
+            if (Audio.musicEnabled)
+            {
+                Audio.ResumeBackgroundMusic();
+            }
+            else
+            {
+                Audio.PauseBackgroundMusic();
+            }
+            UpdateUI_Setting();
+        });
+
+        btnClose.onClick.AddListener(() => {
+            btnClose.enabled = false;
+            onClose();
+        });
+    }
 
 	private void OnEnable()
 	{
-		btnSound.onClick.RemoveAllListeners();
-		btnHaptic.onClick.RemoveAllListeners();
-		btnMusic.onClick.RemoveAllListeners();
-        btnClose.onClick.RemoveAllListeners();
-
-		btnSound.onClick.AddListener(() => { 
-
-		});
-
-        btnHaptic.onClick.AddListener(() => { 
-
-		});
-
-        btnMusic.onClick.AddListener(() => { 
-
-		});
-
-        btnClose.onClick.AddListener(() => {
-			btnClose.enabled = false;
-            onClose();
-        });
+		
     }
 
 	private void OnDisable()
@@ -57,7 +76,10 @@ public class UI_Setting : Dialog<UI_Setting>
 
 	private void UpdateUI_Setting()
 	{
-	}
+        imgSound.sprite = HelperManager.DataPlayer.isPlaySound ? sprSounds[0] : sprSounds[1];
+        imgMusic.sprite = HelperManager.DataPlayer.isPlayMusic ? sprMusics[0] : sprMusics[1];
+        imgVirabtion.sprite = HelperManager.DataPlayer.isVbration ? sprHaptics[0] : sprHaptics[1];
+    }
     public static void Show()
     {
         Open();
