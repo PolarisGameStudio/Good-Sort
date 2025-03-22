bd63892db9793f1b0331e86cb16904e7763b5293
NhanLe1999 <55460318+NhanLe1999@users.noreply.github.com>
2025-03-22 17:41:30 +0700

    update

--- Assets/DevGame/Scripts/Powerup/PowerupFreeze.cs
+++ Assets/DevGame/Scripts/Powerup/PowerupFreeze.cs
@@ -1,4 +1,6 @@
+using DG.Tweening;
 using Spine.Unity;
+using System.Collections;
 using UnityEngine;
 
 public class PowerupFreeze : MonoBehaviour
@@ -18,15 +20,28 @@ public class PowerupFreeze : MonoBehaviour
 
 	private void OnEnable()
 	{
+        anim.gameObject.SetActive(true);
+        anim.transform.position += new Vector3(0, 2, 0);
         var sx = LogicGame.sizeCamera.x / (w * 2);
-        var sy = LogicGame.sizeCamera.y / (h * 2);
-        fx.localScale = new Vector3(sx, sy, 1);
+		var sy = LogicGame.sizeCamera.y / (h * 2);
+		fx.localScale = new Vector3(sx, sy, 1);
         anim.AnimationState.SetAnimation(0, "animation", false);
+        anim.transform.DOMove(Vector3.zero, 0.25f).SetEase(Ease.InOutQuad).OnComplete(() => {
+        });
+		StartCoroutine(DelayDisable());
+    }
+
+	IEnumerator DelayDisable()
+	{
+		yield return new WaitForSeconds(2.0f);
+        anim.gameObject.SetActive(false);
+
     }
 
     private void OnDisable()
 	{
-	}
+		anim.transform.position += new Vector3(0, 2, 0);
+    }
 
 	private void EndGame(EndGameState state)
 	{
