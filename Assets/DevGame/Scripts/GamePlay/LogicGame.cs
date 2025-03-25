3ca00e34e3cb5d6d980a014e29d64c98d097e759
NhanLe1999 <55460318+NhanLe1999@users.noreply.github.com>
2025-03-25 09:17:52 +0700

    Update LogicGame.cs

--- Assets/DevGame/Scripts/GamePlay/LogicGame.cs
+++ Assets/DevGame/Scripts/GamePlay/LogicGame.cs
@@ -120,6 +120,7 @@ public class LogicGame : Singleton<LogicGame>
         // OnSkilFreeze();
         //OnSkillSwap();
         // OnPlayAnimationReplay();
+        HelperManager.DataPlayer.LevelID++;
         StartCoroutine(LoadData());
         isGameOver = false;
     }
